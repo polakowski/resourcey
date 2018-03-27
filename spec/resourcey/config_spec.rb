f5fd7cbbbd44f6a3332181cdd1c70e07068f1dc9
marekpolakowski <marek.polakowski@gmail.com>
2018-03-27 11:49:30 +0200

    RES-7 allow to configure controller parent

--- spec/resourcey/config_spec.rb
+++ spec/resourcey/config_spec.rb
@@ -2,6 +2,7 @@ describe Resourcey::Config do
   describe 'default config' do
     it 'sets default variables values' do
       expect(Resourcey.config.default_paginator).to eq(:paged)
+      expect(Resourcey.config.controller_parent).to eq(ActionController::Base)
     end
   end
 
@@ -12,6 +13,32 @@ describe Resourcey::Config do
       }.to change {
         Resourcey.config.default_paginator
       }.from(:paged).to(:foobar)
+    end
+
+    describe 'controller_parent' do
+      after { Resourcey.config = Resourcey::Config.new }
+
+      it 'sets class if string passed' do
+        expect {
+          Resourcey.configure { |config| config.controller_parent = 'FakeParentController' }
+        }.to change {
+          Resourcey.config.controller_parent
+        }.from(ActionController::Base).to(FakeParentController)
+      end
+
+      it 'sets class if class passed' do
+        expect {
+          Resourcey.configure { |config| config.controller_parent = FakeParentController }
+        }. to change {
+          Resourcey.config.controller_parent
+        }.from(ActionController::Base).to(FakeParentController)
+      end
+
+      it 'raises error if invalid parent name' do
+        expect {
+          Resourcey.configure { |config| config.controller_parent = 1 }
+        }.to raise_error(Resourcey::Errors::RuntimeError)
+      end
     end
   end
 end
